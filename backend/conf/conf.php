bce1647bc4e946713fb42b9962fc578391ce2ecf
francescosalamone <30647430+francescosalamone@users.noreply.github.com>
2018-03-04 11:25:34 +0100

    Add files via upload

--- backend/conf/conf.php
+++ backend/conf/conf.php
@@ -1,1 +1,12 @@
-
+<?php
+ 
+// Include DAL (Data Access Layer)
+require_once(dirname(dirname(__FILE__)) . '/inc/class/DAL.php');
+ 
+// Database
+define ( 'DB_HOST', '127.0.0.1' );
+define ( 'DB_USER', 'root' );
+define ( 'DB_PASSWORD', '' );
+define ( 'DB_NAME', 'timesheet_revisio' );
+ 
+?>
