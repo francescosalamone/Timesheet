3c5970093fe0b3db5be1963e6fbf56f9341ac398
francescosalamone <30647430+francescosalamone@users.noreply.github.com>
2018-03-04 11:23:38 +0100

    Add files via upload

--- backend/language/english.php
+++ backend/language/english.php
@@ -1,1 +1,11 @@
+<?php
+define ( 'ADD_FIRST_PROJECT', 'Hey there! <br /> You haven\'t any project yet. <br />Add your first project!' );
+define ( 'PROJECT_NAME_LABEL', 'Project name:' );
+define ( 'TOTAL_TIME_WORKED', '<br />Time worked for this project: ' );
+define ( 'NEW_SESSION', '<br />Record a new session' );
 
+
+
+
+define ( 'DB_ERROR', 'Database connection error: ' );
+?>
