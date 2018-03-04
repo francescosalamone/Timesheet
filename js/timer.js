3a6e6a8bacf62d88f63510013723ea86f6115f18
francescosalamone <30647430+francescosalamone@users.noreply.github.com>
2018-03-04 11:31:25 +0100

    Add files via upload

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,35 @@
 
+    var start = new Date();
+	var now = 0;
+	var timerId = 0;
+	var startedTime = '';
+
+	startedTime = document.currentScript.getAttribute('starttime');
+
+	if(startedTime != ''){
+
+      	var timestamp = new Date(startedTime.replace(' ', 'T'));
+    	start = timestamp;
+    }
+	timerId = setInterval(count,1000);
+
+	function count(){
+		now = new Date();
+      	var timeDiff = new Date(now - start);
+      	var secs = timeDiff.getSeconds();
+
+      	var mins = timeDiff.getMinutes();
+      	var hours = timeDiff.getHours()-1;
+
+      	if(secs < 10){
+          secs = "0" + secs;
+        }
+      	if(mins < 10){
+          mins = "0" + mins;
+        }
+      	if(hours < 10){
+          hours = "0" + hours;
+        }
+
+      	document.getElementById("message").innerHTML = hours + ":" + mins + ":" + secs;
+    }
